611a7e0354710284ca47207b03443fa688501f19
nkear003 <nkearney757@gmail.com>
2017-06-22 12:38:03 +0200

    Fixed download script

--- download_script.php
+++ download_script.php
@@ -2,8 +2,9 @@
 
 $download = $_POST['download'];
 
-$path = '/home/fuonyagency/webapps/dev_prime_php/downloads/' . $download . '.zip'; // the file made available for download via this PHP file
+$path = 'https://devedge.steyoyoke.com/downloads/' . $download . '.zip'; // the file made available for download via this PHP file
 $mm_type="application/octet-stream"; // modify accordingly to the file type of $path, but in most cases no need to do so
+$headers  = get_headers($path, 1);
 
 header("Pragma: public");
 header("Expires: 0");
@@ -11,9 +12,9 @@ header("Cache-Control: must-revalidate, post-check=0, pre-check=0");
 header("Cache-Control: public");
 header("Content-Description: File Transfer");
 header("Content-Type: " . $mm_type);
-header("Content-Length: " .(string)(filesize($path)) );
+header('Content-Length: '.($headers['Content-Length']));
 header('Content-Disposition: attachment; filename="'.basename($path).'"');
-header("Content-Transfer-Encoding: binary\n");
+header("Content-Transfer-Encoding: binary");
 
 readfile($path); // outputs the content of the file
 
